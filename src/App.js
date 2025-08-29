a1d3e0daef67858f5be8ca653442f1b5c0bdfc2c
raufali11 <alizadarauf1@gmail.com>
2025-08-29 12:47:53 +0400

    Fix dependencies and prepare for reinstall

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./App.scss";
 import Main from "./containers/Main";
+import LanguageSwitcher from "./components/LanguageSwitcher";
 
 function App() {
   return (
     <div>
       <Main />
+      <LanguageSwitcher />
     </div>
   );
 }
 
 export default App;
+
--- src/containers/twitter-embed/twitter.js
+++ src/containers/twitter-embed/twitter.js
@@ -1,22 +1,8 @@
-import React, {Suspense, useContext} from "react";
+import React, {useContext} from "react";
 import "./twitter.scss";
-import Loading from "../loading/Loading";
-import {TwitterTimelineEmbed} from "react-twitter-embed";
+import {Timeline} from "react-twitter-widgets";
 import {twitterDetails} from "../../portfolio";
 import StyleContext from "../../contexts/StyleContext";
-
-const renderLoader = () => <Loading />;
-const cantDisplayError =
-  "<div className='centerContent'><h2>Can't load? Check privacy protection settings</h2></div>";
-
-function timeOut() {
-  setTimeout(function () {
-    if (!document.getElementById("twitter").innerHTML.includes("iframe")) {
-      document.getElementById("twitter").innerHTML = cantDisplayError;
-    }
-  }, 10000);
-}
-var widthScreen = window.screen.width;
 
 export default function Twitter() {
   const {isDark} = useContext(StyleContext);
@@ -29,24 +15,20 @@ export default function Twitter() {
   }
   if (twitterDetails.userName) {
     return (
-      <Suspense fallback={renderLoader()}>
-        <div className="tw-main-div" id="twitter">
-          <div className="centerContent">
-            <TwitterTimelineEmbed
-              sourceType="profile"
-              screenName={twitterDetails.userName}
-              options={{height: 400, width: {widthScreen}}}
-              placeholder={renderLoader()}
-              autoHeight={false}
-              borderColor="#fff"
-              key={isDark ? "1" : "2"}
-              theme={isDark ? "dark" : "light"}
-              noFooter={true}
-              onload={timeOut()}
-            />
-          </div>
+      <div className="tw-main-div" id="twitter">
+        <div className="centerContent">
+          <Timeline
+            dataSource={{
+              sourceType: "profile",
+              screenName: twitterDetails.userName
+            }}
+            options={{
+              height: "400",
+              theme: isDark ? "dark" : "light"
+            }}
+          />
         </div>
-      </Suspense>
+      </div>
     );
   } else {
     return null;
